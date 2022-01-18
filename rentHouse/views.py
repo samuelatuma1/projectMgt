a5f890b22a2bfa79950be7bacbd72d11e933a740
samuelatuma1 <atumasaake@gmail.com>
2022-01-18 05:39:24 +0100

    Set up django Home page

--- rentHouse/views.py
+++ rentHouse/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world")
