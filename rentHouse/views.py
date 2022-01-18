b3fbbdfeff69c6058078d9619df49d1c2984574c
samuelatuma1 <atumasaake@gmail.com>
2022-01-18 05:56:26 +0100

    Created static folder, and base template

--- rentHouse/views.py
+++ rentHouse/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, 'renthouse/index.html')
